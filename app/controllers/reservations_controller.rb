a147c7bd9458c169be0cb12131f199e97f86dd83
Nicolas Helary <helary.nicolas@gmail.com>
2022-06-08 16:55:47 +0200

    fixfix

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,7 +12,8 @@ class ReservationsController < ApplicationController
     @reservation.gnome = @gnome
     @reservation.user = current_user
     if @reservation.save
-      redirect_to reservation_path(@reservation)
+      #redirect_to reservation_path(@reservation)
+      redirect_to gnomes_path
     else
       render :new
     end
