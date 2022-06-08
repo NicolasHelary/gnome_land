89149d77f56aa2db683789e2215f8fedb4f3a215
Nicolas Helary <helary.nicolas@gmail.com>
2022-06-08 14:28:28 +0200

    model et controller de reservation

--- app/models/gnome.rb
+++ app/models/gnome.rb
@@ -1,5 +1,6 @@
 class Gnome < ApplicationRecord
   belongs_to :user
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
   validates :description, length: { minimum: 15 }, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :gnomes
+  resources :gnomes do
+    resources :reservations, only: %i[new create destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
