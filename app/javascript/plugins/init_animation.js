abe9c0ada80315b6fe3bafd1f52275fdfd7ffb05
Romualdo Delain <romualdo.delain@outlook.com>
2022-06-09 14:40:24 +0200

    added images for js

---
+++ app/javascript/plugins/init_animation.js
@@ -0,0 +1,13 @@
+const animationTrololo = () => {
+
+
+
+  var audio = document.getElementById("audio");
+  document.onclick = function() {
+    audio.play();
+  }
+}
+  // var audio = document.getElementById(“audio”);
+  // audio.addEventListener(“click”, (event) => {
+  // })
+export {animationTrololo}
